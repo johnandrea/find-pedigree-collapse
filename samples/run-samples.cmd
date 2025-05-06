cb6657bfe41f245ea9ef53b5e9fef1233f3005fc
johnandrea <46268748+johnandrea@users.noreply.github.com>
2025-05-06 13:34:21 -0300

    Output of test files

---
+++ samples/run-samples.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+if exist r.err del r.err
+
+find-pedigree-collapse.py --iditem=refn --personid 1 cousins-plus.ged >cousins-plus.out 2>>r.err 
+find-pedigree-collapse.py --iditem=refn --personid 1 half-cousins.ged >half-cousins.out 2>>r.err
+find-pedigree-collapse.py --iditem=refn --personid 1 multiple-plus.ged >multiple-plus.out 2>>r.err
+find-pedigree-collapse.py --iditem=refn --personid 1 sibling-child.ged >sibling-child.out 2>>r.err
+
+find-pedigree-collapse.py --dates --iditem=refn --personid 1 charles-ii.ged  >charles-ii.out 2>>r.err
+
+find-pedigree-collapse.py --dates --iditem=refn --personid 5 large-royals-2023.ged  >large-royals-2023.out 2>>r.err
