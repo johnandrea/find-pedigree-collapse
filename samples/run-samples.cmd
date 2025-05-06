e75ca0b467e9fb79013733705de0e277f3cf7fef
johnandrea <46268748+johnandrea@users.noreply.github.com>
2025-05-06 14:05:08 -0300

    Rename to use country name

--- samples/run-samples.cmd
+++ samples/run-samples.cmd
@@ -7,6 +7,6 @@ find-pedigree-collapse.py --iditem=refn --personid 1 half-cousins.ged >half-cous
 find-pedigree-collapse.py --iditem=refn --personid 1 multiple-plus.ged >multiple-plus.out 2>>r.err
 find-pedigree-collapse.py --iditem=refn --personid 1 sibling-child.ged >sibling-child.out 2>>r.err
 
-find-pedigree-collapse.py --dates --iditem=refn --personid 1 charles-ii.ged  >charles-ii.out 2>>r.err
+find-pedigree-collapse.py --dates --iditem=refn --personid 1 charles-ii-of-spain.ged  >charles-ii-of-spain.out 2>>r.err
 
-find-pedigree-collapse.py --dates --iditem=refn --personid 5 large-royals-2023.ged  >large-royals-2023.out 2>>r.err
+find-pedigree-collapse.py --dates --iditem=refn --personid 5 british-royals-2023.ged  >british-royals-2023.out 2>>r.err
