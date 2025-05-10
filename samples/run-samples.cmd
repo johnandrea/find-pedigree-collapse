27b0ac37633e6a3af5f23b5d9f03b5b0ca14c446
johnandrea <46268748+johnandrea@users.noreply.github.com>
2025-05-10 19:16:52 -0300

    Ecample of no collapse

--- samples/run-samples.cmd
+++ samples/run-samples.cmd
@@ -6,7 +6,9 @@ find-pedigree-collapse.py --iditem=refn --personid 1 cousins-plus.ged >cousins-p
 find-pedigree-collapse.py --iditem=refn --personid 1 half-cousins.ged >half-cousins.out 2>>r.err
 find-pedigree-collapse.py --iditem=refn --personid 1 multiple-plus.ged >multiple-plus.out 2>>r.err
 find-pedigree-collapse.py --iditem=refn --personid 1 sibling-child.ged >sibling-child.out 2>>r.err
+find-pedigree-collapse.py --iditem=refn --personid 1 none.ged >sibling-child.out 2>>r.err
 
-find-pedigree-collapse.py --dates --iditem=refn --personid 1 charles-ii-of-spain.ged  >charles-ii-of-spain.out 2>>r.err
 
-find-pedigree-collapse.py --dates --iditem=refn --personid 5 british-royals-2023.ged  >british-royals-2023.out 2>>r.err
+find-pedigree-collapse.py --dates --iditem=refn --personid 1 charles-ii.ged  >charles-ii.out 2>>r.err
+
+find-pedigree-collapse.py --dates --iditem=refn --personid 5 large-royals-2023.ged  >large-royals-2023.out 2>>r.err
